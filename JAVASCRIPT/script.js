b9c80c342cd242314817044509dbb2434597441a
N1D0 <nicomanci28@gmail.com>
2024-12-23 15:45:28 -0300

    Resposive nav and visuals

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,22 +1,34 @@
-const toggleButton = document.getElementById('toggle-btn')
-const sidebar = document.getElementById('sidebar')
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
 
 function toggleSidebar() {
-    sidebar.classList.toggle('close')
-    toggleButton.classList.toggle('rotate')
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
 
-    Array.from(sidebar.getElementByClassName('show')).forEach(ul => { 
-        ul.classList.remove('show')
-        ul.previusElementSibling.classList.remove('rotate')
-    })
+    closeAllSubMenus();
 }
 
 function toggleSubMenu(button) {
+
+    // Cierra todos los submenús si otro está siendo abierto
+    if(!button.nextElementSibling.classList.contains('show')) {
+        closeAllSubMenus();
+        }
+
+    // Alterna el estado del submenú actual
     button.nextElementSibling.classList.toggle('show')
     button.classList.toggle('rotate')
 
+    // Si el sidebar está cerrado, lo abre al interactuar con un submenú
     if(sidebar.classList.contains('close')){
         sidebar.classList.toggle('close')
         toggleButton.classList.toggle('rotate')
     }
+}
+
+function closeAllSubMenus(){
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => { 
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
+    })
 }
